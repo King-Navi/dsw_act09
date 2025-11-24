e8f3d334d7fdca05d33d02e6e8c42826a184a05b
King-Navi <168604098+King-Navi@users.noreply.github.com>
2025-11-24 14:27:21 -0600

    Fixed

--- random-meal.js
+++ random-meal.js
@@ -19,31 +19,62 @@ async function loadRandomMeal() {
     const meal = data.meals[0];
 
     section.innerHTML = `
-      <div class="articles">
-        <article>
-          <figure>
-            <img src="${meal.strMealThumb}" alt="${meal.strMeal}">
-          </figure>
-          <div class="article-preview">
-            <h2>${meal.strMeal}</h2>
-            <p>
-              ${truncate(meal.strInstructions, 260)}
-            </p>
-            <p>
-              <strong>Categoría:</strong> ${meal.strCategory || 'N/A'}<br>
-              <strong>Área:</strong> ${meal.strArea || 'N/A'}
-            </p>
-            <a href="${meal.strSource || '#'}" class="read-more" title="Ver receta completa" target="_blank">
-              Ver receta completa
-            </a>
-          </div>
-        </article>
+      <div class="random-meal-wrapper">
+        <div class="articles">
+          <article>
+            <figure>
+              <img src="${meal.strMealThumb}" alt="${meal.strMeal}">
+            </figure>
+            <div class="article-preview">
+              <h2>${meal.strMeal}</h2>
+              <p>
+                ${truncate(meal.strInstructions, 260)}
+              </p>
+              <p>
+                <strong>Categoría:</strong> ${meal.strCategory || 'N/A'}<br>
+                <strong>Área:</strong> ${meal.strArea || 'N/A'}
+              </p>
+              <a href="${meal.strSource || '#'}" class="read-more" title="Ver receta completa" target="_blank">
+                Ver receta completa
+              </a>
+            </div>
+          </article>
+        </div>
+
+        <button id="new-random-meal" class="btn-random-meal" type="button">
+          Buscar otra comida 🍕
+        </button>
       </div>
     `;
+
+    const reloadButton = document.getElementById('new-random-meal');
+    reloadButton.addEventListener('click', loadRandomMeal);
+
   } catch (error) {
     console.error(error);
     section.innerHTML = '<p>Ocurrió un error al cargar la comida random.</p>';
   }
 }
 
+const toggleMenu = () => {
+  const navigation = document.querySelector(".navigation");
+
+  const burgerMenu = document.querySelector(".menu-icon");
+  const src = burgerMenu.getAttribute("src");
+
+  const isBurger = src === "assets/burger-menu.svg";
+  const iconName = isBurger ? "assets/close.svg" : "assets/burger-menu.svg";
+
+  burgerMenu.setAttribute("src", iconName);
+
+  if (!isBurger) {
+    navigation.classList.add("navigation--mobile--fadeout");
+    setTimeout(() => {
+      navigation.classList.toggle("navigation--mobile");
+    }, 300);
+  } else {
+    navigation.classList.remove("navigation--mobile--fadeout");
+    navigation.classList.toggle("navigation--mobile");
+  }
+};
 document.addEventListener('DOMContentLoaded', loadRandomMeal);
